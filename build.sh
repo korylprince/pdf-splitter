abf5f5d94ce3d59c926b1531f8e4f04f99d848be
Kory Prince <korylprince@gmail.com>
2019-11-21 14:38:48 -0600

    build i386

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ rm -Rf build
 mkdir build
 
 for GOOS in darwin linux windows; do
-    #386 currently broken in UniDoc
-    #for GOARCH in 386 amd64; do
-    for GOARCH in amd64; do
+    for GOARCH in 386 amd64; do
         export GOOS
         export GOARCH
         if [ $GOOS = "windows" ]; then
